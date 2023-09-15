11dd80e7ca97917c060057e74e978791a3ed07fc
Michael Kim <michaelkim009@gmail.com>
2023-09-14 19:47:34 -0700

    FastAPI test run

---
+++ README.md
@@ -0,0 +1,9 @@
+Why use FastAPI
+
+1. Just plain Python
+2. Built in Async -- ASGI
+3. Built in Data Validation
+4. Typed Python
+5. Errors in JSON
+6. Authentication methods built-in
+7. Swagger and ReDoc built-in 
---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from models import Todo
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+# Just saving here
+todos = []
+
+# Get all todos
+@app.get("/todos")
+async def get_todos():
+    return {"todos": todos}
+
+# Get single todo
+@app.get("/todos/{todo_id}")
+async def get_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            return {"todo" : todo}
+    return {"message" : "No todos found"}
+
+# Create a todo
+@app.post("/todos")
+async def create_todos(todo: Todo):
+    todos.append(todo)
+    return {"message": "Todo has been addded"}
+
+# Update a todo
+@app.put("/todos/{todo_id}")
+async def update_todo(todo_id: int, todo_obj: Todo):
+    for todo in todos:
+        if todo.id == todo_id:
+            todo.id = todo_obj.id
+            todo.item = todo_obj.item
+            return {"todo" : todo }
+    return {"message" : "No todos found"}
+
+# Delete a todo
+@app.delete("/todos/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todos:
+        if todo.id == todo_id:
+            todos.remove(todo)
+            return {"message" : "Todo has been deleted!"}
+    return {"message" : "No todos found"}
+
+# Supports HTTP Basic OAuth2 tokens (JWT tokens) and header API keys.
